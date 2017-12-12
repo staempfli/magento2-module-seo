a66bdf5f3caa15f1b661f76eaa3f6e15e00944ce
Marcel Hauri <marcel@hauri.me>
2017-12-12 20:33:46 +0100

    [task] use custom bootstrap for test to check __() method without full magento installation

---
+++ Test/Unit/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+function __()
+{
+    $argc = func_get_args();
+
+    $text = array_shift($argc);
+    if (!empty($argc) && is_array($argc[0])) {
+        $argc = $argc[0];
+    }
+
+    return new \Magento\Framework\Phrase($text, $argc);
+}
+
+require_once __DIR__ . '/../../vendor/autoload.php';
