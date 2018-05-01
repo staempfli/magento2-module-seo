b34cd0b7902444f6e4df447f8f36530d8be8333b
Marcel Hauri <marcel@hauri.me>
2018-05-01 11:58:15 +0200

    [task] add additional test to router plugin

--- Plugin/RouteConfigPlugin.php
+++ Plugin/RouteConfigPlugin.php
@@ -7,10 +7,12 @@ use Magento\Framework\App\Route\Config as Subject;
 
 class RouteConfigPlugin
 {
+    const MODULE_NAME = 'Staempfli_Seo';
+
     public function afterGetModulesByFrontName(Subject $subject, $result, $frontName)
     {
-        if ($frontName === 'robots') {
-            return [0 => 'Staempfli_Seo'];
+        if ($frontName === 'robots' && in_array(self::MODULE_NAME, $result)) {
+            return [self::MODULE_NAME];
         }
         return $result;
     }
