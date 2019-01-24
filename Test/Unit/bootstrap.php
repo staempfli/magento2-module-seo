61259d21a94e2f261c9b0723b6e2af171ca6e432
Marcel Hauri <marcel@hauri.me>
2019-01-24 22:06:58 +0100

    [fix] tests and missing theme version

--- Test/Unit/bootstrap.php
+++ Test/Unit/bootstrap.php
@@ -1,15 +1,2 @@
 <?php
-
-function __()
-{
-    $argc = func_get_args();
-
-    $text = array_shift($argc);
-    if (!empty($argc) && is_array($argc[0])) {
-        $argc = $argc[0];
-    }
-
-    return new \Magento\Framework\Phrase($text, $argc);
-}
-
 require_once __DIR__ . '/../../vendor/autoload.php';
