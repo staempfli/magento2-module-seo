9b305428b69f47785dd1252622b9b71b17c39d8a
bst2002git <bst2002@ios.dhs.org>
2020-02-05 17:10:44 +0100

    Update Robots.php
    show Sitemaps per Store View

--- Model/Robots.php
+++ Model/Robots.php
@@ -36,16 +36,23 @@ class Robots
      */
     private $storeResolver;
 
+		/**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
     public function __construct(
         Config $config,
         Sitemap $sitemap,
         WebsiteRepositoryInterface $websiteRepository,
-        StoreResolverInterface $storeResolver
+        StoreResolverInterface $storeResolver,
+				\Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         $this->config = $config;
         $this->sitemap = $sitemap;
         $this->websiteRepository = $websiteRepository;
         $this->storeResolver = $storeResolver;
+				$this->_storeManager = $storeManager;
     }
 
     public function getContent() : string
@@ -74,18 +81,25 @@ class Robots
         $data = [];
         $sitemaps = $this->sitemap
             ->getCollection()
-            ->addFieldToFilter('store_id', ['in' => array_keys($website->getStores())]);
+            //->addFieldToFilter('store_id', ['in' => array_keys($website->getStores())]);
+            ->addFieldToFilter('store_id', ['in' => $this->_storeManager->getStore()->getId()]);
 
         if (!$sitemaps) {
             return $data;
         }
 
         foreach ($sitemaps as $sitemap) {
+						/*
             $data[] = sprintf(
                 'Sitemap: %s%s%s',
                 rtrim($website->getDefaultStore()->getBaseUrl(UrlInterface::URL_TYPE_DIRECT_LINK), '/'),
                 $sitemap->getSitemapPath(),
                 $sitemap->getSitemapFilename()
+            );
+						*/
+						$data[] = sprintf(
+                'Sitemap: %s',
+                rtrim($sitemap->getSitemapUrl($sitemap->getSitemapPath(),$sitemap->getSitemapFilename()), '/')
             );
         }
         return $data;
